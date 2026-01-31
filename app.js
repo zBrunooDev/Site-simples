ca85ebf242ac199b5318a0fdee3e600884aaee5f
zBrunooDev <zbrunolima513@gmail.com>
2026-01-31 01:11:53 -0300

    Projeto de site simples efetuado através do curso da fundação Bradesco

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+
+
+    var className = document.body.className;
+    if(className == "light-theme"){
+        this.textContent = "Dark";
+    }
+    else{
+        this.textContent = "Light";
+    }
+    console.log('current classs name: ' + className);
+});
